251f9a5460cb2b37c2ee1f6015b534f22401092f
Sasha Varlamov <sasha@sashavarlamov.com>
2015-05-29 11:53:18 -0400

    Add styles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,10 +3,10 @@
 
 // Declare app level module which depends on filters, and services
 // Declare app level module which depends on filters, and services
-angular.module('ckllsApp', ['cklls.filters', 'ckllsApp.services', 'ckllsApp.directives','ngRoute']).
+angular.module('salApp', ['sal.filters', 'salApp.services', 'salApp.directives','ngRoute']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partial/1', controller: ckllsCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partial/2', controller: ckllsCtrl2});
+    $routeProvider.when('/view1', {templateUrl: 'partial/1', controller: salCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partial/2', controller: salCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
   }]);
